1634b072576bce05a1fdd452d6c3d78365aed1ab
hafdhisofien <1114@holbertonschool.com>
2019-09-24 12:06:39 +0000

    big one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * print_alphabet - entry point
+ * sofien
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{ int i;
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+}
