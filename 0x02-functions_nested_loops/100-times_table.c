cc7fe6c49fe087ffa8b17c68cca4a5833d216917
hafdhisofien <1114@holbertonschool.com>
2019-09-24 17:02:54 +0000

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,48 @@
 #include "holberton.h"
+
 /**
- * print_times_table - print n times the tables
- * @n: where start table
+ * print_times_table - prints the n times table, starting at 0.
  *
- *Return: nothing
+ * @n: input integer
+ *
+ * Returns - void
  */
 void print_times_table(int n)
+{int a, b, ab, huns, tens, ones;
+if (!(n > 15 || n < 0))
 {
-int r, c;
-int h, d, u, mul;
-if (n >= 0 && n <= 15)
+for (a = 0; a <= n; a++)
 {
-for (r = 0; r <= n; r++)
+if (n == 0)
 {
-for (c = 0; c <= n; c++)
-{
-mul = r * c;
-h = mul / 100;
-d = mul / 10 % 10;
-u = mul % 10;
-if (h == 0 && c != 0)
-{
-_putchar(' ');
-if (d == 0)
-_putchar(' ');
-else
-_putchar(d + '0');
-_putchar(u + '0');
-}
-else if (h != 0)
-{
-_putchar(h + '0');
-_putchar(d + '0');
-_putchar(u + '0');
-}
-if (c == 0)
 _putchar('0');
-if (c != n)
+_putchar('\n');
+break;
+}
+for (b = 0; b <= n; b++)
 {
+if (b == 0)
+{
+_putchar(0 + '0');
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+ab = a * b;
+huns = (ab > 99) ? ab / 100 : -16;
+tens = (ab > 99) ? ab / 10  % 10 : (ab > 9) ? ab / 10 : -16;
+ones = ab % 10;
+_putchar(huns + '0'); /* -16 + '0' is a SPACE */
+_putchar(tens + '0'); /* -16 + '0' is a SPACE */
+_putchar(ones + '0');
+if (b == n)
+break;
 _putchar(',');
 _putchar(' ');
 }
 }
 _putchar('\n');
-c = 0;
 }
 }
 }
