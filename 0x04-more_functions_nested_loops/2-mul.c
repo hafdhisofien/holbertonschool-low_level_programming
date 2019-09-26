0e15c90ce1934f12ac093748afe1836a058a5cc0
hafdhisofien <1114@holbertonschool.com>
2019-09-26 13:17:41 +0000

    yep

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * mul - multiples two numbers
+ * @a: int a
+ * @b: int b
+ * Return: product
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_numbers - prints 0 to 9
+ *
+ * Return: nothing;
+ */
+void print_numbers(void)
+{
+int count;
+for (count = 0; count < 10; count++)
+{
+_putchar(count + '0');
+}
+_putchar('\n');
+}
