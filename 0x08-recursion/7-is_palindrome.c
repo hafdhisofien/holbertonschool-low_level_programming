a6822d10eca9c5826c4eecb52a9a9d9efd4d8073
hafdhisofien <1114@holbertonschool.com>
2019-10-09 19:00:07 +0000

    fix

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,44 +1,47 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
- * is_palindrome  - check the code for Holberton School students.
- *@s: string
- * Return: Always 0.
+ * _strlen_recursion - Finds the length of the string to be tested
+ * @s: The string to test
+ * Return: The length of the string
  */
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+else
+return (_strlen_recursion(s + 1) + 1);
+}
+
+/**
+ * palindrome_checker - function for recursive palindrome algo
+ * @s: String to check
+ * @start: First char of string to check
+ * @end: Last char of string to check
+ *
+ * Return: 1 if str is palindrome, 0 otherwise
+ */
+
+int palindrome_checker(char *s, int start, int end)
+{
+if (s[start] != s[end])
+return (0);
+if (start >= end)
+return (1);
+return (palindrome_checker(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - Tests whether a string is a palindrome
+ * @s: The string to test
+ * Return: 1 if palindrome, 0 otherwise
+ */
+
 int is_palindrome(char *s)
 {
-int i = _lent(s);
-int k = 0;
-return (pal(s, i - 1, k));
-}
-
-/**
- * _lent - function mesuring the lenght of the string
- *@s: string
- * Return: int
- */
-
-int _lent(char *s)
-{
-if (*s)
-{
-s++;
-return (1 + _lent(s));
-}
+int end = _strlen_recursion(s);
+if (!palindrome_checker(s, 0, end - 1))
 return (0);
-}
-/**
- *pal - comparing strings the original and the reversed
- *@i: int
- *@k: int
- *@str: reversed string
- *Return: int
- */
-
-int pal(char *str, int i, int k)
-{
-if (k > i)
 return (1);
-if (str[i] == str[k])
-return (pal(str, i - 1, k + 1));
-return (0);
 }
