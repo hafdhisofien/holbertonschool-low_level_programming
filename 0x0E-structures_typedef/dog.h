9c17a7268a6fb0a43798571bc8436ec4e8ce95c3
hafdhisofien <1114@holbertonschool.com>
2019-10-21 17:37:26 +0000

    pup

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,10 +9,10 @@
  */
 typedef struct dog
 {
-  char *name;
-  float age;
-  char *owner;
-} dog_t;
+char *name;
+float age;
+char *owner;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
