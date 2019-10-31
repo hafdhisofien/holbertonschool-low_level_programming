ca60f21bbaf51cec75bbced79cc14e1de8be328a
hafdhisofien <1114@holbertonschool.com>
2019-10-31 16:33:21 +0000

    first

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns length of list
+ * @h: pointer to head of list
+ * Return: length of list
+ **/
+
+size_t list_len(const list_t *h)
+{
+size_t len = 0;
+const list_t *walk;
+walk = h;
+while (walk)
+{
+walk = walk->next;
+len++;
+}
+return (len);
+}
