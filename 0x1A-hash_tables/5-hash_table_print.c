db836eaa3cea77ebd91b9c56591482f5125e4d11
hafdhisofien <1114@holbertonschool.com>
2020-01-23 10:09:37 +0000

    fix

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,27 +1,29 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_print -  function that prints a hash table.
- * @ht: the hashed table
- * Return: dict of the hashed table
+ * hash_table_print - function that prints a hash table
+ * @ht: hash table
+ * returns: dict of the hashed table in format.
  */
 void hash_table_print(const hash_table_t *ht)
 {
-unsigned long int i = 0;
-hash_node_t *tmp = NULL;
-if (ht == NULL)
-return;
+unsigned long int i = 0, flag = 0;
+hash_node_t  *tmp = NULL;
 
+if (!ht)
+return;
 printf("{");
 while (i < ht->size)
 {
 tmp = ht->array[i];
-while (tmp)
+while (tmp != NULL)
 {
-printf("'%s' : '%s'", tmp->key, tmp->value);
-if (ht->array[i + 1] == NULL)
-break;
-printf(", ");
+if (flag == 0)
+{
+printf("'%s': '%s'", tmp->key, tmp->value);
+flag = 1;
+}
+else
+printf(", '%s': '%s'", tmp->key, tmp->value);
 tmp = tmp->next;
 }
 i++;
